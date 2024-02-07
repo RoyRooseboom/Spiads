c5eb7ef7c3d59f73aae7905e7c30cfa2769c5454
RoyRooseboom <brrooseboom@gmail.com>
2024-02-07 09:43:26 +0100

    first commit

---
+++ spiads/src/main/java/nl/roy/spiads/SpiadsApplication.java
@@ -0,0 +1,13 @@
+package nl.roy.spiads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiadsApplication.class, args);
+	}
+
+}
---
+++ spiads/src/test/java/nl/roy/spiads/SpiadsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.roy.spiads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
