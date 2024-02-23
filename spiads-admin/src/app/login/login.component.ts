2adf279a0d1911499c341aa5a59daff2b1915928
RoyRooseboom <brrooseboom@gmail.com>
2024-02-23 09:07:40 +0100

    Inlog pagina gestyled

--- spiads-admin/src/app/login/login.component.ts
+++ spiads-admin/src/app/login/login.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  user: { gebruikersnaam: string; wachtwoord: string } = {
+    gebruikersnaam: '',
+    wachtwoord: '',
+  };
+
+  verplichtText = 'Ingevulde waarde is onjuist';
+
+  send() {}
+}
