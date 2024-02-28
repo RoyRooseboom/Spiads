b0c84d892e53e7cc42d4130a22c1360ed99fb49c
RoyRooseboom <brrooseboom@gmail.com>
2024-02-28 13:56:55 +0100

    restjes Tekstveld

--- spiads-frontend/src/app/app.module.ts
+++ spiads-frontend/src/app/app.module.ts
@@ -7,15 +7,9 @@ import { InschrijvenComponent } from './inschrijven/inschrijven.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SignaturePadComponent } from './shared/signature-pad/signature-pad.component';
 import { FormsModule } from '@angular/forms';
-import { TekstveldComponent } from './tekstveld/tekstveld.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InschrijvenComponent,
-    SignaturePadComponent,
-    TekstveldComponent,
-  ],
+  declarations: [AppComponent, InschrijvenComponent, SignaturePadComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [SignaturePadComponent],
   bootstrap: [AppComponent],
